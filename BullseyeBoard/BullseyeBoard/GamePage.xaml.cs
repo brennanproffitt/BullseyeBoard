b90c2bf170f2ac64dc9ad3d9328c6aa8b7e92f84
Brennan Proffitt <bmproffitt@gus.pittstate.edu>
2019-11-18 18:11:50 -0600

    I think we got the incremental logic for normal rounds on the game page.

--- BullseyeBoard/BullseyeBoard/GamePage.xaml.cs
+++ BullseyeBoard/BullseyeBoard/GamePage.xaml.cs
@@ -15,6 +15,15 @@ namespace BullseyeBoard
         private int teamAScore;
         private int teamBScore;
 
+        private string currentTeam = "Team 1";
+        private int throwValue = 0;
+
+        private string team1NameSetting = "TeamOneName";
+        private string team2NameSetting = "TeamTwoName";
+
+        private int currentRound = 1;
+        private int counter = 1;
+
         public GamePage()
         {
             InitializeComponent();
@@ -22,7 +31,42 @@ namespace BullseyeBoard
             teamAScore = Preferences.Get("startingGameScore", 0);
             teamBScore = Preferences.Get("startingGameScore", 0);
 
-            currentTeamLabel.Text = Preferences.Get("TeamOneName","Team 1");
+            currentTeamLabel.Text = Preferences.Get(team1NameSetting,"Team 1");
+
+
+        }
+
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+            if (currentTeam == "Team 1")
+            {
+                teamAScore -= throwValue;
+                Preferences.Set("teamAScore", teamAScore);
+                currentTeamScore.Text = teamAScore.ToString();
+                counter++;
+                if(counter > 3)
+                {
+                    currentTeam = "Team 2";
+                    currentTeamLabel.Text = Preferences.Get(team2NameSetting,"Team 2");
+                    currentTeamScore.Text = teamBScore.ToString();
+                    counter = 1;
+                }
+            }
+            else 
+            {
+                teamBScore -= throwValue;
+                Preferences.Set("teamBScore", teamBScore);
+                currentTeamScore.Text = teamBScore.ToString();
+                counter++;
+                if (counter > 3)
+                {
+                    currentTeam = "Team 1";
+                    currentTeamLabel.Text = Preferences.Get(team1NameSetting, "Team 1");
+                    currentTeamScore.Text = teamAScore.ToString();
+                    counter = 1;
+                }
+            }
+
 
         }
     }
