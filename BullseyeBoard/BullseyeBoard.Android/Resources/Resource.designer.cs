5ab1662f8d148a6ccf95e44fa9f62669f0c3427c
brennanproffitt <bmproffitt@gus.pittstate.edu>
2019-12-13 21:58:33 -0600

    Experimenting with font and design

--- BullseyeBoard/BullseyeBoard.Android/Resources/Resource.designer.cs
+++ BullseyeBoard/BullseyeBoard.Android/Resources/Resource.designer.cs
@@ -6447,6 +6447,9 @@ namespace BullseyeBoard.Droid
 			// aapt resource value: 0x7F07013B
 			public const int tooltip_frame_light = 2131165499;
 			
+			// aapt resource value: 0x7F07013C
+			public const int woodbackground = 2131165500;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
